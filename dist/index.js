151b76efe20c27484aa0affc9e8e61e28f5c531a
Fabio Yuiti Goto <lab@yuiti.com.br>
2018-11-06 07:49:30 -0200

    Quick fix

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,39 @@
-/**
- * @yuigoto/validators
- * ----------------------------------------------------------------------
- * A really, really simple validation library.
- *
- * This file is here just because (and because I'm kinda lazy to nuke it).
- *
- * @author    Fabio Y. Goto <lab@yuiti.com.br>
- * @since     0.0.1
- */
 "use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Email", {
+  enumerable: true,
+  get: function get() {
+    return _Email2.default;
+  }
+});
+Object.defineProperty(exports, "Cpf", {
+  enumerable: true,
+  get: function get() {
+    return _Cpf2.default;
+  }
+});
+Object.defineProperty(exports, "Cnpj", {
+  enumerable: true,
+  get: function get() {
+    return _Cnpj2.default;
+  }
+});
+Object.defineProperty(exports, "Pis", {
+  enumerable: true,
+  get: function get() {
+    return _Pis2.default;
+  }
+});
+
+var _Email2 = _interopRequireDefault(require("./general/Email"));
+
+var _Cpf2 = _interopRequireDefault(require("./documents/Cpf"));
+
+var _Cnpj2 = _interopRequireDefault(require("./documents/Cnpj"));
+
+var _Pis2 = _interopRequireDefault(require("./documents/Pis"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
--- src/index.js
+++ src/index.js
@@ -8,3 +8,7 @@
  * @author    Fabio Y. Goto <lab@yuiti.com.br>
  * @since     0.0.1
  */
+export Email from "./general/Email";
+export Cpf from "./documents/Cpf";
+export Cnpj from "./documents/Cnpj";
+export Pis from "./documents/Pis";
